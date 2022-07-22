a6925620a69e1cdd068d113da98fcb27fb55c527
PattonChandler <Patton.Chandler@stern.nyu.edu>
2022-07-21 19:45:59 -0500

    Tests are passing

--- game.py
+++ game.py
@@ -20,13 +20,13 @@ def winner(user_choice, computer_choice):
 
     elif user_choice == "paper" and computer_choice == "rock":
         #print("The computer wins")
-        return "The computer wins"
+        return "The user wins"
     elif user_choice == "paper" and computer_choice == "paper":
         #print("It's a tie!")
         return "It's a tie!"
     elif user_choice == "paper" and computer_choice == "scissors":
         #print("The user wins")
-        return "The user wins"
+        return "The computer wins"
 
     elif user_choice == "scissors" and computer_choice == "rock":
         #print("The computer wins")
@@ -58,24 +58,4 @@ if __name__ == "__main__":
     #
     # DETERMINATION OF WINNER
     #
-
-    if u == "rock" and c == "rock":
-        print("It's a tie!")
-    elif u == "rock" and c == "paper":
-        print("The computer wins")
-    elif u == "rock" and c == "scissors":
-        print("The user wins")
-
-    elif u == "paper" and c == "rock":
-        print("The computer wins")
-    elif u == "paper" and c == "paper":
-        print("It's a tie!")
-    elif u == "paper" and c == "scissors":
-        print("The user wins")
-
-    elif u == "scissors" and c == "rock":
-        print("The computer wins")
-    elif u == "scissors" and c == "paper":
-        print("The user wins")
-    elif u == "scissors" and c == "scissors":
-        print("It's a tie!")
+    print(winner(u, c))
